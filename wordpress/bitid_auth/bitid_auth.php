f8bb396d60dfd5f23486af0cde6668bbc3cd7eac
Puggan Se <from_git@puggan.se>
2014-07-02 07:57:30 +0200

    Starting to build a Wordpress-plugin

---
+++ wordpress/bitid_auth/bitid_auth.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Bitid Authentication
+ * @author Puggan
+ * @version 0.0.0-2014-07-02
+ */
+/*
+Plugin Name: Bitid Authentication
+Description: Bitid Authentication, extends wordpress default authentication with the bitid-protocol
+Version: 0.0.0-2014-07-02
+Author: Puggan
+Author URI: http://blog.puggan.se
+*/
+
+DEFINE("BITID_AUTHENTICATION_PLUGIN_VERSION",'0.0.2');
---
+++ wordpress/bitid_auth/readme.txt
@@ -0,0 +1,10 @@
+Plugin Name: Bitid Authentication
+Description: Bitid Authentication, extends wordpress default authentication with the bitid-protocol
+Version: 0.0.0-2014-07-02
+Author: Puggan
+Author URI: http://blog.puggan.se
+
+Version log:
+
+0.0.0-2014-07-02
+	An empty shell, booking the name
